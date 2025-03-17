f3445a5564fa057251084e5f2e97e5136a9c70e6
alexsant22 <alexandre.santana2201@gmail.com>
2025-03-17 14:51:49 -0300

    Ex2 - Arrays

---
+++ Exercicios_Aula/JS - 06/02/js/script.js
@@ -0,0 +1,38 @@
+let arrayNumero = [];
+
+function exibirNumero () {
+    let saida = document.getElementById('resultado');
+    if (saida instanceof HTMLParagraphElement) {
+        // Ordenando o array de numeros
+        arrayNumero.sort((a, b) => a -b);
+
+        // Exibindo
+        saida.textContent = arrayNumero.join(', ');
+    }
+}
+
+function cadastrarNumero (numero) {
+    // Verifica se é nulo e se não é um número
+    if (numero && !isNaN(numero)) {
+        arrayNumero.push(Number(numero)); // Adiciona no tipo numero
+        exibirNumero();
+    } else {
+        alert("Por favor, insira um valor válido!");
+    }
+}
+
+const configurarNumeros = () => {
+    let entradaNumero = document.getElementById('entradaNumero');
+    let cadastrarNumeroBtn = document.getElementById('cadastrarNumeroBtn');
+
+    //prettier-ignore
+    if ((cadastrarNumeroBtn instanceof HTMLButtonElement) &&
+        (entradaNumero instanceof HTMLInputElement)) {
+            cadastrarNumeroBtn.addEventListener('click', () => {
+                cadastrarNumero(entradaNumero.value);
+                entradaNumero.value = '';
+            })
+        }
+}
+
+document.addEventListener('DOMContentLoaded', configurarNumeros);
