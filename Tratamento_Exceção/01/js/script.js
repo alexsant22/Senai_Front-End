e221abc86b36fc546ea6ef60ff337663ad35b278
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-04-08 11:43:57 -0300

    Finalizado

--- Tratamento_Exceção/01/js/script.js
+++ Tratamento_Exceção/01/js/script.js
@@ -1,3 +1,16 @@
+function configurar() {
+    let botaoAcessar = document.getElementById('acessarBtn');
+    let nomeInput = document.getElementById('nomeInput');
+    let idadeInput = document.getElementById('idadeInput');
+
+    if ((botaoAcessar instanceof HTMLButtonElement) && (nomeInput instanceof HTMLInputElement) && (idadeInput instanceof HTMLInputElement)) {
+        botaoAcessar.addEventListener('click', () => {
+            let idade = parseInt(idadeInput.value);
+            validarIdade(idade);
+        });
+    }
+}
+
 function exibirErro(mensagem) {
     let saida = document.getElementById('saida');
 
@@ -6,12 +19,42 @@ function exibirErro(mensagem) {
     }
 }
 
-function validarIdade() {
+function exibirSaida(nome = "Usuário") {
+    let saida = document.getElementById('saida');
+    let nomeInput = document.getElementById('nomeInput');
+
+    if ((saida instanceof HTMLDivElement) && (nomeInput instanceof HTMLInputElement)) {
+        let nomeFinal = nomeInput.value.trim() || nome;
+
+        saida.textContent = `Acesso do ${nomeFinal}, autorizado!`
+    }
+}
+
+function validarIdade(idadeUsuario) {
     let nomeInput = document.getElementById('nomeInput');
     let idadeInput = document.getElementById('idadeInput');
 
     if ((nomeInput instanceof HTMLInputElement) && (idadeInput instanceof HTMLInputElement)) {
         let nome = nomeInput.value;
         let idade = parseInt(idadeInput.value);
+
+        if (nome == "") {
+            nome = "Usuário";
+        }
+
+        try {
+            if (isNaN(idade)) {
+                throw new Error('Digite um número válido');
+            }
+            if (idade < 18) {
+                throw new Error(`Acesso negado, ${nome} é menor de idade`);
+            }
+
+            exibirSaida();
+        } catch (error) {
+            exibirErro(error.message);
+        }
     }
 }
+
+document.addEventListener('DOMContentLoaded', configurar);
