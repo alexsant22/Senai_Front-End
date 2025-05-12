43c110a8ebaf0bf5da3b87876c7a8bde72837ae4
alexsant22 <alexandre.santana2201@gmail.com>
2025-05-12 16:53:36 -0300

    01 - Regex

---
+++ Regex/01/js/script.js
@@ -0,0 +1,46 @@
+function configurar() {
+    let nomeInput = document.getElementById('nome');
+    let confirmarBtn = document.getElementById('confirmarBtn');
+
+    if ((nomeInput instanceof HTMLInputElement) && (confirmarBtn instanceof HTMLButtonElement)) {
+        confirmarBtn.addEventListener('click', () => {
+            let resultado = verificarNome(nomeInput.value);
+            exibirConteudo(resultado);
+
+            nomeInput.value = '';
+        });
+    }
+}
+
+function verificarNome(nome) {
+    try {
+        if (!nome || nome == null || nome == " ") {
+            throw new Error('Digite um nome válido');
+        }
+
+        let negacao = /[^a-zA-ZÀ-ÿ \s]/g;
+        
+        if (negacao.test(nome)) {
+            throw new Error('O nome não pode conter números ou caracteres especiais');
+        }
+
+        return nome;
+
+    } catch (error) {
+        alert(`Erro: ${error.message}.`)
+    }
+}
+
+function exibirConteudo(resultado) {
+    let saida = document.getElementById('resultado');
+
+    if (saida instanceof HTMLParagraphElement) {
+        if (!resultado) {
+            saida.textContent = "";
+        } else {
+            saida.textContent = `Nome: ${resultado} - Verificado!`;
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', configurar);
