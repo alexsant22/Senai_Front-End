db7d486a7fb4bb82c0513e556516e975301fefa0
alexsant22 <alexandre.santana2201@gmail.com>
2025-05-19 17:30:01 -0300

    Async - Base_Produtos

--- Async's/base_produtos/js/script_v1.js
+++ Async's/base_produtos/js/script_v1.js
@@ -5,3 +5,35 @@ let produtos = [
     { nome: 'HeadSet', diretorio: 'headSet', textoDescritivo: 'Headset Gamer Com Mic Quantum Jbl  - Preto', imagens: ['01.jpg', '02.jpg', '03.jpg'] },
     { nome: 'Teclado', diretorio: 'teclado', textoDescritivo: 'Teclado Mecânico Gamer Phantom, ABNT2 - Preto', imagens: ['01.jpg', '02.jpg', '03.jpg'] },
   ];
+
+function inicializarProdutos() {
+  let containerPrincipal = document.getElementById('produtos');
+
+  if(containerPrincipal instanceof HTMLDivElement) {
+    produtos.forEach((produto) => {
+      console.log(produto);
+
+      let containerProduto = criarContainerProduto(produto);
+    });
+  }
+}
+
+function criarContainerProduto(produto) {
+  let divProduto = document.createElement('div');
+  divProduto.className = 'container';
+
+  let quadro = criarQuadro(produto);
+  divProduto.appendChild(quadro);
+
+  return divProduto;
+}
+
+function criarQuadro(produto) {
+  produto.imagens.forEach((item) => {
+   let imagem = document.createElement('img');
+
+   imagem.src = `img/${produto.diretorio}/${item}`;
+  });
+}
+
+  document.addEventListener('DOMContentLoaded', inicializarProdutos);
